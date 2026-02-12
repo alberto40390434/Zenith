23b1ec5efa4492944ca0662601b5dc36cb3543c2
alberto40390434 <a42944053@gmail.com>
2026-02-11 17:05:12 -0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,7 @@
-importScripts("/scram/scramjet.wasm.js");
-importScripts("/scram/scramjet.codecs.js");
-importScripts("/scram/scramjet.config.js");
+import { scramjetPath } from "@mercuryworkshop/scramjet/path";
 
-self.$scramjet = {
-    config: {
-        prefix: "/service/",
-        codec: "xor",
-        files: {
-            wasm: "/scram/scramjet.wasm.wasm",
-            worker: "/scram/scramjet.worker.js",
-            client: "/scram/scramjet.client.js",
-            shared: "/scram/scramjet.shared.js",
-            sync: "/scram/scramjet.sync.js",
-        },
-    },
-};
-
-self.ScramjetServiceWorker.prototype.route = function(event) {
-    return event.request.url.startsWith(location.origin + self.$scramjet.config.prefix);
-};
-
-self.addEventListener("fetch", (event) => {
-    const sw = new ScramjetServiceWorker();
-    if (sw.route(event)) {
-        event.respondWith(sw.fetch(event));
-    }
+fastify.register(fastifyStatic, {
+	root: scramjetPath,
+	prefix: "/scram/",
+	decorateReply: false,
 });
